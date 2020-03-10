ca2515bb1da64ea0279fb3b29df237c500388d96
Gab2904 <gabriel.r.martins@outlook.com>
2020-03-10 20:33:46 -0300

    Versao 1.2 - Classe Aluno com Sets

--- Aluno.java
+++ Aluno.java
@@ -1,16 +1,28 @@
 
-public class Aluno {
+public class Aluno{
+   private String nome;
+   private int id;
+   
+   
+public Aluno(String nome, int id) {
 	
-	String nome;
-	int id;
+	this.nome = "Gabriel";
+	this.id = 1;
 	
-	public String getNome() {
-		return nome;
-	}
 	
-	public int getId() {
-		return id;
-	}
+}
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
 }
 
-
+   
+}
